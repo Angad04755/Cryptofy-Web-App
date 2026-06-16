284d6e0c0498cbbc71a3ef8e1b640c520e26844d
= <=>
2026-06-17 01:50:29 +0530

    changes

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,47 +1,54 @@
 import { useNavigate } from "react-router-dom";
 import LoginModal from "../../auth/LoginModal";
 import { useState } from "react";
+import video from "../../assets/stock-footage-financial-market-trends-analyzing-stock-price-movements-with-graphical-data-representations-over.mp4";
+
 const Home = () => {
   const navigate = useNavigate();
-    const [openModal, setOpenmodal] = useState(false)
+  const [openModal, setOpenmodal] = useState(false);
+
   return (
-    <section
-      className="w-full min-h-screen bg-gradient-to-b from-green-400 to-red-400 flex items-center justify-center"
-      style={{
-        animation: "Pulse 4s ease-in-out infinite",
-      }}
-    >
-      <article className="text-center px-6">
-        <h1 className="text-4xl md:text-7xl font-bold text-white mb-6">
-          Track Crypto in Real-Time
-        </h1>
+    <>
+      {/* Background Video */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="fixed w-full min-h-screen object-cover z-[-10]"
+      >
+        <source src={video} type="video/mp4" />
+      </video>
 
-        <p className="text-lg md:text-xl text-black font-semibold mb-8">
-          Monitor prices, analyze trends, stay ahead of the market.
-        </p>
+      {/* Content */}
+      <section className="min-h-screen flex items-center justify-center">
+        <article className="text-center px-6">
+          <h1 className="text-4xl md:text-7xl font-bold text-green-100 mb-6">
+            Track Crypto in Real-Time
+          </h1>
 
-        <div className="flex place-content-center gap-5">
-          <button
-          
-            
-            className="w-fit bg-white px-5 py-2 text-black rounded-xl font-semibold cursor-pointer transition hover:bg-white/80"
-            onClick={() =>setOpenmodal(true)}
-          >
-            Login
-          </button>
-          <button className="bg-black text-white px-7 py-3 text-lg rounded-xl cursor-pointer hover:bg-black/80 transition font-semibold"
-            onClick={() => navigate("/prices")}
-          >
-          View Prices
-          </button>
-          
-        </div>
-      </article>
-      {openModal && (
-      <LoginModal onclose={() => setOpenmodal(false)}/>
-      )}
-    </section>
-    
+          <div className="flex justify-center gap-5">
+            <button
+              className="bg-white hover:bg-white/90 px-5 py-2 text-black rounded-xl font-semibold cursor-pointer transition"
+              onClick={() => setOpenmodal(true)}
+            >
+              Login
+            </button>
+
+            <button
+              className="bg-black text-white px-7 py-3 rounded-xl font-semibold cursor-pointer"
+              onClick={() => navigate("/prices")}
+            >
+              View Prices
+            </button>
+          </div>
+        </article>
+
+        {openModal && (
+          <LoginModal onclose={() => setOpenmodal(false)} />
+        )}
+      </section>
+    </>
   );
 };
 
